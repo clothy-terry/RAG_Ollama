efcf388a8ec9f4b90fb0d365219ce8667f5d5a25
TerryLiu <terryliu@berkeley.edu>
2024-07-28 11:54:44 -0500

    initial commit

--- test3.py
+++ test3.py
@@ -1,8 +1,8 @@
 import os
 
-os.environ["LANGCHAIN_TRACING_V2"] = "true"
-os.environ["LANGCHAIN_ENDPOINT"] = "https://api.smith.langchain.com"
-os.environ["LANGCHAIN_API_KEY"] = "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"
+#os.environ["LANGCHAIN_TRACING_V2"] = "true"
+#os.environ["LANGCHAIN_ENDPOINT"] = "https://api.smith.langchain.com"
+#os.environ["LANGCHAIN_API_KEY"] = "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"
 #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
 
 from langchain_community.vectorstores import Chroma
